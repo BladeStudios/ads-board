df55cdab9b6e5a9c2746c9986eb5df12c7f256ff
Blade Studios <maciejwarakomski@gmail.com>
2022-10-30 20:55:57 +0100

    Add "Add ad" form in ad/add view

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\AdFormType;
 
 class AdController extends AbstractController
 {
     /**
      * @Route("/ad/add", name="ad_add")
      */
-    public function add(): Response
+    public function add(Request $request): Response
     {
-        return $this->render('ad/add.html.twig');
+        $ad = new Ad();
+        $form = $this->createForm(AdFormType::class, $ad);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            dump($form->getData());
+        }
+        return $this->render('ad/add.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
