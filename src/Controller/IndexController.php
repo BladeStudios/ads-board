ff217b87ee6bbe7c34bd0fdda2bc1224e7e19d33
Blade Studios <maciejwarakomski@gmail.com>
2022-10-30 15:05:07 +0100

    Change default route for index() method
    Change also route name and remove passed parameter

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -9,12 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/index", name="app_index")
+     * @Route("/", name="index")
      */
     public function index(): Response
     {
-        return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
-        ]);
+        return $this->render('index/index.html.twig');
     }
 }
