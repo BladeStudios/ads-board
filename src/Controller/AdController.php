bd9defeaaa72dde5818f3edd20a293ae46349e43
Blade Studios <maciejwarakomski@gmail.com>
2022-11-01 18:06:20 +0100

    Implement sending "Dodawanie ogłoszenia" form
    Add flash message if ad was successfully added

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -8,20 +8,25 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use App\Form\AdFormType;
+use Doctrine\Persistence\ManagerRegistry;
 
 class AdController extends AbstractController
 {
     /**
      * @Route("/ad/add", name="ad_add")
      */
-    public function add(Request $request): Response
+    public function add(Request $request, ManagerRegistry $doctrine): Response
     {
         $ad = new Ad();
         $form = $this->createForm(AdFormType::class, $ad);
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid())
         {
-            dump($form->getData());
+            $entityManager = $doctrine->getManager();
+            $entityManager->persist($ad);
+            $entityManager->flush();
+            $this->addFlash('success', 'Pomyślnie dodano ogłoszenie!');
+            return $this->redirectToRoute('ad_add');
         }
         return $this->render('ad/add.html.twig', [
             'form' => $form->createView()
