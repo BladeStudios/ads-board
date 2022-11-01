957cee91e81635068b70b583a85c33c7f6f2831f
Blade Studios <maciejwarakomski@gmail.com>
2022-11-01 18:39:30 +0100

    Change route name from "index" to "home"

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
